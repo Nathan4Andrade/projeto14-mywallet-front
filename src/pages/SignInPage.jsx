6d10e3ffdfe871d509d1898bde793a254a82a5a5
Nathan4Andrade <nathanandrade20@gmail.com>
2023-07-04 14:26:07 -0300

    feat: routes

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,22 +1,79 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-import MyWalletLogo from "../components/MyWalletLogo"
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignInPage() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function signin(e) {
+    e.preventDefault();
+    setLoading(!loading);
+    const loginInfo = {
+      email,
+      password,
+    };
+    navigate("/home");
+    /*  axios
+      .post(`${BASE_URL}auth/login`, loginInfo)
+      .then((resp) => {
+        setToken(resp.data.token);
+        setUser(resp.data);
+        persistUser(resp.data);
+        navigate("/home");
+      })
+      .catch(() => {
+        alert("Usuário não encontrado");
+        setLoading(false);
+      }); */
+  }
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signin}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={loading}
+          required
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autoComplete="new-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={loading}
+          required
+        />
+        <button disabled={loading}>
+          {loading ? (
+            <ThreeDots
+              height="25"
+              width="30"
+              radius="9"
+              color="#ffffff"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              wrapperClassName=""
+              visible={true}
+            />
+          ) : (
+            <span>Entrar</span>
+          )}
+        </button>
       </form>
 
-      <Link>
-        Primeira vez? Cadastre-se!
-      </Link>
+      <Link to={`/cadastro`}>Primeira vez? Cadastre-se!</Link>
     </SingInContainer>
-  )
+  );
 }
 
 const SingInContainer = styled.section`
@@ -25,4 +82,9 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+  button {
+    div {
+      justify-content: center;
+    }
+  }
+`;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,24 +1,113 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function SignUpPage() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function signup(e) {
+    e.preventDefault();
+    setLoading(!loading);
+    const loginInfo = {
+      email,
+      password,
+    };
+    navigate("/");
+    /*  axios
+      .post(`${BASE_URL}auth/login`, loginInfo)
+      .then((resp) => {
+        setToken(resp.data.token);
+        setUser(resp.data);
+        persistUser(resp.data);
+        navigate("/home");
+      })
+      .catch(() => {
+        alert("Usuário não encontrado");
+        setLoading(false);
+      }); */
+  }
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signup}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          placeholder="Nome"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          disabled={loading}
+          required
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={loading}
+          required
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autoComplete="new-password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            if (e.target.value !== confirmPassword) {
+              console.log("senhas diferentes");
+              setDisable(true);
+            }
+          }}
+          disabled={loading}
+          required
+        />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          autoComplete="new-password"
+          value={confirmPassword}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+            if (e.target.value !== password) {
+              console.log("senhas diferentes");
+              setDisable(true);
+            } else {
+              setDisable(false);
+            }
+          }}
+          disabled={loading}
+          required
+        />
+        {disable ? <p>Senhas incompatíveis</p> : ""}
+        <button disabled={loading || disable}>
+          {loading ? (
+            <ThreeDots
+              height="25"
+              width="30"
+              radius="9"
+              color="#ffffff"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              wrapperClassName=""
+              visible={true}
+            />
+          ) : (
+            <span>Cadastrar</span>
+          )}
+        </button>
       </form>
 
-      <Link>
-        Já tem uma conta? Entre agora!
-      </Link>
+      <Link to={`/`}>Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
-  )
+  );
 }
 
 const SingUpContainer = styled.section`
@@ -27,4 +116,14 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+  form {
+    > p {
+      color: #ff7676;
+    }
+  }
+  button {
+    :disabled {
+      background-color: grey;
+    }
+  }
+`;
